0d9fea89ae5feab8fdf92056fc0a0c24a8eab925
Prince Priyadarshi <prince.priyadarshi@beatoapp.com>
2022-10-09 18:44:46 +0530

    innit

--- Hand Tracking/HandTrarking.py
+++ Hand Tracking/HandTrarking.py
@@ -12,8 +12,8 @@ mpDraw = mp.solutions.drawing_utils
 
 while True:
     success, img =cap.read()
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results=hands.process(imgRGB)
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) #we need to convert img into RGB beacuse hand class only uses RGB img
+    results=hands.process(imgRGB) #now we are process that img
 
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
