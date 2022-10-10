3b384a923d4f26b297e2ca4e1856448f70914400
Prince Priyadarshi <prince.priyadarshi@beatoapp.com>
2022-10-10 20:49:30 +0530

    innit

--- Hand Tracking/HandTrarkingModule.py
+++ Hand Tracking/HandTrarkingModule.py
@@ -1,66 +1,67 @@
+import cv2
 import time
 import mediapipe as mp
-import cv2
-
 
 class handDetector():
-    def __init__(self,mode=False, maxHands = 2, detectionCon =0.5, trackCon = 0.5):
+    def __init__(self, mode = False, maxHands = 2, detectionCon = 0.5, trackCon = 0.5):
         self.mode = mode
-        self.maxhands = maxHands
+        self.maxHands = maxHands
         self.detectionCon = detectionCon
         self.trackCon = trackCon
 
         self.mpHands = mp.solutions.hands
-        hands = self.mpHands.Hands(self.mode,self.maxhands,self.detectionCon,
-                                    self.trackCon)
-        mpDraw = mp.solutions.drawing_utils
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.detectionCon, self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
 
-cap =cv2.VideoCapture(0)
+    def findHands(self, img, draw = True) :
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.result = self.hands.process(imgRGB)
+        # print(result.multi_hand_landmarks)
 
-mpHands = mp.solutions.hands
-hands=mpHands.Hands()
-mpDraw = mp.solutions.drawing_utils
+        if self.result.multi_hand_landmarks :
+            for handLand in self.result.multi_hand_landmarks :
+                if draw :
+                    self.mpDraw.draw_landmarks(img, handLand, self.mpHands.HAND_CONNECTIONS)
 
-pTime=0 #pervious time
-cTime=0 #current time
+        return img
 
-
-while True:
-    success, img =cap.read()
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) #we need to convert img into RGB beacuse hand class only uses RGB img
-    results=hands.process(imgRGB) #now we are process that img
-
-    if results.multi_hand_landmarks:
-        for handLms in results.multi_hand_landmarks:
-            for id,lm in enumerate(handLms.landmark):
-                #print(id,lm) #this is hand landmarks
-                h, w, c =img.shape #h=height of img w=widhth c= chnanel
-                cx, cy = int(lm.x*w), int(lm.y*h) #for finding postions
-                print(id,cx,cy) #this is id of node at x and y 
-                #if id==0:
-                cv2.circle(img,(cx,cy),25,(255,0,255),cv2.FILLED) #we are tracking by drawing circle
-
-            mpDraw.draw_landmarks(img, handLms,mpHands.HAND_CONNECTIONS) #if we just use 
-                                                                            #handLms it will only show dot but if we also 
-                                                                            # use mpHands.HAND_CONNECTIONS then it show connection between dots
-
-    cTime= time.time()
-    fps = 1/(cTime - pTime)
-
-    pTime = cTime
-
-    cv2.putText(img,str(int(fps)),(10,70),cv2.FONT_HERSHEY_COMPLEX_SMALL,3,(255,0,255),3)
+    def findPosition(self, img, handNo = 0, draw = True) :
+        PosList = []
+        if self.result.multi_hand_landmarks :
+            myHand = self.result.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(myHand.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                PosList.append([id, cx, cy])
+                
+                if draw :
+                    cv2.circle(img, (cx,cy), 10, (255,255,255), cv2.FILLED)
+            
+        return PosList
 
 
-    cv2.imshow("Image",img)
-    cv2.waitKey(1)
+def main():
+    pTime = 0
+    cTime = 0
+    cap = cv2.VideoCapture(0)
+    detector = handDetector()
 
+    while True :
+        success, img = cap.read()
+        img = detector.findHands(img)
+        PosList = detector.findPosition(img)
 
+        if len(PosList) != 0 :
+            print(PosList[4])
 
+        cTime = time.time()
+        fps = 1/(cTime - pTime)
+        pTime = cTime
 
+        cv2.putText(img, str(int(fps)), (10,70), cv2.FONT_HERSHEY_DUPLEX, 3, (255,0,0), 3)
 
+        cv2.imshow("Webcam", img)
+        cv2.waitKey(1)
 
-
-
-
-
+if __name__ == "__main__" :
+    main()
